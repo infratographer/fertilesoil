27e53cdb4ee5243af27ac1c4cea01cb2a9045cd8
Juan Antonio Osorio <juan.osoriorobles@eu.equinix.com>
2022-12-13 08:44:56 +0200

    Add option for faster read-only driver using exact staleness reads
    This leverages CRDB's follower reads to add the option to create a
    read-only server that reads from the nearest follower instead of
    following the leader.
    
    This will hopefully lead to faster/lighter reads with the caveat that
    reads are not fully consistent with the latest state.
    
    Signed-off-by: Juan Antonio Osorio <juan.osoriorobles@eu.equinix.com>

--- storage/crdb/driver/readonlydriver_test.go
+++ storage/crdb/driver/readonlydriver_test.go
@@ -3,6 +3,7 @@ package driver_test
 import (
 	"context"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 
@@ -12,28 +13,69 @@ import (
 	"github.com/infratographer/fertilesoil/storage/crdb/utils"
 )
 
+const (
+	// ironically, testing fast reads is slow since we have to wait for
+	// the follower read timestamp to be consistent with the latest data.
+	waitConsistency = 5 * time.Second
+)
+
+var readonlyTestCases = []struct {
+	name         string
+	inconsistent bool // fast queries are inconsistent
+}{
+	{
+		"consistent",
+		false,
+	},
+	{
+		"inconsistent",
+		true,
+	},
+}
+
 func TestReaderGetOneRoot(t *testing.T) {
 	t.Parallel()
 
 	db := utils.GetNewTestDB(t, baseDBURL)
-	adminstore := driver.NewDirectoryAdminDriver(db)
-	rostore := driver.NewDirectoryReaderDriver(db)
+	adminstore := driver.NewDirectoryDriver(db)
 
 	rd := withRootDir(t, adminstore)
 
-	// retrieve from db
-	queryrootdir, qerr := rostore.GetDirectory(context.Background(), rd.ID)
-	assert.NoError(t, qerr, "error querying db")
-	assert.Equal(t, rd.ID, queryrootdir.ID, "id should match")
-	assert.Equal(t, rd.Name, queryrootdir.Name, "name should match")
+	for _, tc := range readonlyTestCases {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			var store *driver.Driver
+
+			opts := []driver.Options{driver.WithReadOnly()}
+			if tc.inconsistent {
+				opts = append(opts, driver.WithFastReads())
+				time.Sleep(waitConsistency)
+			}
+
+			store = driver.NewDirectoryDriver(db, opts...)
+
+			// retrieve from db
+			queryrootdir, qerr := store.GetDirectory(context.Background(), rd.ID)
+			assert.NoError(t, qerr, "error querying db")
+			assert.Equal(t, rd.ID, queryrootdir.ID, "id should match")
+			assert.Equal(t, rd.Name, queryrootdir.Name, "name should match")
+
+			// list roots returns only the root directory
+			roots, err := store.ListRoots(context.Background())
+			assert.NoError(t, err, "error listing roots")
+			assert.Len(t, roots, 1, "should only be one root")
+			assert.Equal(t, rd.ID, roots[0], "id should match")
+		})
+	}
 }
 
 func TestReaderGetOneDirectory(t *testing.T) {
 	t.Parallel()
 
 	db := utils.GetNewTestDB(t, baseDBURL)
-	adminstore := driver.NewDirectoryAdminDriver(db)
-	rostore := driver.NewDirectoryReaderDriver(db)
+	adminstore := driver.NewDirectoryDriver(db)
 
 	rd := withRootDir(t, adminstore)
 
@@ -49,56 +91,67 @@ func TestReaderGetOneDirectory(t *testing.T) {
 	assert.Equal(t, d.Name, retd.Name, "name should match")
 	assert.Equal(t, d.Parent, retd.Parent, "parent id should match")
 
-	// retrieve from db
-	querydir, qerr := rostore.GetDirectory(context.Background(), d.ID)
-	assert.NoError(t, qerr, "error querying db")
-	assert.Equal(t, d.ID, querydir.ID, "id should match")
+	for _, tc := range readonlyTestCases {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
 
-	// get as child of root
-	children, err := rostore.GetChildren(context.Background(), rd.ID)
-	assert.NoError(t, err, "error getting children")
-	assert.Len(t, children, 1, "should be one child")
-	assert.Equal(t, d.ID, children[0], "id should match")
+			var store *driver.Driver
 
-	// get parent
-	parent, err := rostore.GetParents(context.Background(), d.ID)
-	assert.NoError(t, err, "error getting parents")
-	assert.Len(t, parent, 1, "should be one parent")
-	assert.Equal(t, rd.ID, parent[0], "id should match")
+			opts := []driver.Options{driver.WithReadOnly()}
+			if tc.inconsistent {
+				opts = append(opts, driver.WithFastReads())
+				time.Sleep(waitConsistency)
+			}
+
+			store = driver.NewDirectoryDriver(db, opts...)
+
+			// retrieve from db
+			querydir, qerr := store.GetDirectory(context.Background(), d.ID)
+			assert.NoError(t, qerr, "error querying db")
+			assert.Equal(t, d.ID, querydir.ID, "id should match")
+
+			// get as child of root
+			children, err := store.GetChildren(context.Background(), rd.ID)
+			assert.NoError(t, err, "error getting children")
+			assert.Len(t, children, 1, "should be one child")
+			assert.Equal(t, d.ID, children[0], "id should match")
+
+			// get parent
+			parent, err := store.GetParents(context.Background(), d.ID)
+			assert.NoError(t, err, "error getting parents")
+			assert.Len(t, parent, 1, "should be one parent")
+			assert.Equal(t, rd.ID, parent[0], "id should match")
+		})
+	}
 }
 
 func TestReaderCannotCreateRootEvenIfCast(t *testing.T) {
 	t.Parallel()
 
 	db := utils.GetNewTestDB(t, baseDBURL)
-	rostore := driver.NewDirectoryReaderDriver(db)
+	rostore := driver.NewDirectoryDriver(db, driver.WithReadOnly())
 
 	d := &v1.Directory{
 		Name: "root",
 	}
 
-	admcast, ok := rostore.(storage.DirectoryAdmin)
-	assert.True(t, ok, "reader should be castable to admin")
-
-	rd, err := admcast.CreateRoot(context.Background(), d)
+	rd, err := rostore.CreateRoot(context.Background(), d)
 	assert.ErrorIs(t, err, storage.ErrReadOnly, "error should be read only")
 	assert.Nil(t, rd, "root directory should be nil")
 }
 
-func TestReaderCannotCreateDirectoryEvenIfCast(t *testing.T) {
+func TestReaderCannotCreateDirectory(t *testing.T) {
 	t.Parallel()
 
 	db := utils.GetNewTestDB(t, baseDBURL)
-	rostore := driver.NewDirectoryReaderDriver(db)
+	rostore := driver.NewDirectoryDriver(db, driver.WithReadOnly())
 
 	d := &v1.Directory{
 		Name: "testdir",
 	}
 
-	admcast, ok := rostore.(storage.DirectoryAdmin)
-	assert.True(t, ok, "reader should be castable to admin")
-
-	rd, err := admcast.CreateDirectory(context.Background(), d)
+	rd, err := rostore.CreateDirectory(context.Background(), d)
 	assert.ErrorIs(t, err, storage.ErrReadOnly, "error should be read only")
 	assert.Nil(t, rd, "directory should be nil")
 }
