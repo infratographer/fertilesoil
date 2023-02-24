ad6fff7c369d8593cb1a2578f0bbdc8028b1a2a5
Mike Mason <mimason@equinix.com>
2023-02-24 14:35:59 +0000

    include missing comment
    Signed-off-by: Mike Mason <mimason@equinix.com>

--- storage/options.go
+++ storage/options.go
@@ -2,12 +2,14 @@ package storage
 
 // Options contains all possible storage options.
 type Options struct {
+	// WithDeletedDirectories includes deleted directories in queries.
 	WithDeletedDirectories bool
 }
 
 // Option defines a storage Options handler.
 type Option func(opts *Options)
 
+// WithDeletedDirectories includes deleted directories in queries.
 var WithDeletedDirectories Option = func(opts *Options) {
 	opts.WithDeletedDirectories = true
 }
